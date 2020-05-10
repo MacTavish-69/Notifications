0e97f5246a2e44fba6e44b59271a9503809e91f3
Mac <tehseenhashmi10@gmail.com>
2020-05-10 21:57:38 +0500

    Added Project

---
+++ app/Notifications/RegistrationSuccessful.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RegistrationSuccessful extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        // This will be sent in mail notification
+        return (new MailMessage)
+                    ->line("Congrats, You have successfully registered on another website. If you can't find login link then
+                                please click on the big damn button we tailored for you!")
+                    ->action('Login Now', url('/login'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+//    public function toArray($notifiable)
+//    {
+//        return [
+//            //
+//        ];
+//    }
+
+    public function toDatabase($notifiable) {
+        // This will be stored in Database, You'll fetch this notification later to display in application
+        return [
+            'body' => 'Yaay! You made it to here! You logged in to view the notification, SO damn Nice!',
+        ];
+    }
+
+}
