bf461147bb4b2b6a926a9bf675e589d149eb75e3
Mac <tehseenhashmi10@gmail.com>
2020-05-10 22:12:20 +0500

    Fixed app.blade.php
    Fixed the accidental commented out section in app.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,6 +25,6 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('home')->with('notifications', $user->notifications);Heyyy
+        return view('home')->with('notifications', $user->notifications);
     }
 }
